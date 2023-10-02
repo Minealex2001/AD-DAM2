2d1660b4d0e414536dfddd64cd364faa3503cbfe
Minealex_ <alejandrosanchezpinto@gmail.com>
2023-10-02 23:14:32 +0200

    Me falta simplemente probarlo

--- Tema-2/Ejercicio-1/src/Main.java
+++ Tema-2/Ejercicio-1/src/Main.java
@@ -1,12 +1,9 @@
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        AplanarDirectorios aplanarDirectorios = new AplanarDirectorios(Paths.get());
+        AplanarDirectorios aplanarDirectorios = new AplanarDirectorios(Paths.get("\\wsl.localhost\\kali-linux\\tmp\\niats"));
         aplanarDirectorios.aplanar();
     }
 }
