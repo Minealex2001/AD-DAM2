877f17d01f1eb327b3977b3d7927b4cd1eb5bb7e
Minealex <alejandrosanchezpinto@gmail.com>
2023-10-05 20:08:35 +0200

    Comienzo el ejercicio 2

--- Tema-2/Ejercicio2/src/Main.java
+++ Tema-2/Ejercicio2/src/Main.java
@@ -1,5 +1,30 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+ * Class MainActivity
+ *
+ * This class is used to create a Main object
+ * and run the program.
+ * @version 1.0
+ * @since 2023-05-10
+ * @author Alejandro Sánchez PInto
+ * @GitHub <a href="https://github.com/Minealex2001/AD-DAM2">...</a>
+ */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        final String COMMA_DELIMITER = ",";
+
+        try (Stream<String> contenidoFichero = Files.lines(Path.of(".", "src", "resources", "funkos.csv"))) {
+            List<Funko> listaCSV = contenidoFichero.map(l -> Arrays.asList(l.split(COMMA_DELIMITER))).map(l -> new Funko(l.get(0), l.get(1), l.get(2), l.get(3), l.get(4))).toList();
+        } catch (IOException e) {
+            e.printStackTrace(System.out);
+        }
 
     }
 }
