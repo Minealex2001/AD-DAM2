f1651ec065cec9a9eaca0aa9ff3aa00bf6d08deb
MInealex <alejandrosanchezpinto@gmail.com>
2023-10-19 16:08:44 +0200

    Restructuracion de las carpetas. (El ejercicio 3 esta ya realizado con Maven)

--- Tema-2/Ejercicio-3/src/main/java/AtletaFemenina.java
+++ Tema-2/Ejercicio-3/src/main/java/AtletaFemenina.java
@@ -1,28 +1,19 @@
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
-
-public class AtletaFemenina {
-    private String nombre;
-    private List<String> prueba;
-    private int edad;
-    private String pais;
-
-    @Getter
+    @Data
     @NoArgsConstructor
     @AllArgsConstructor
 
     @JacksonXmlRootElement(localName = "atleta")
     public class AtletaFemenina{
-        @JacksonXmlProperty{isAttribute = true}
         private String nombre;
-        @JacksonXmlProperty{isAttribute = true}
         private List<String> prueba;
-        @JacksonXmlProperty{isAttribute = true}
         private int edad;
-        @JacksonXmlProperty{isAttribute = true}
         private String pais;
-
     }
 
-
-
-}
