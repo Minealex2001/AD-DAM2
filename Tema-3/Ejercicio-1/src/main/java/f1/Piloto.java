db168dbdd384cfd9a4722e1de510df75c6f6bba0
MInealex <alejandrosanchezpinto@gmail.com>
2023-11-10 15:57:32 +0100

    Ejercicio comentado completamente

--- Tema-3/Ejercicio-1/src/main/java/f1/Piloto.java
+++ Tema-3/Ejercicio-1/src/main/java/f1/Piloto.java
@@ -1,14 +1,17 @@
 package f1;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+/**
+ * Clase Piloto
+ */
 @Data
 @NoArgsConstructor
 public class Piloto {
+    // Atributos
     private int driverId;
     private String driverCode;
     private String driverForename;
@@ -18,6 +21,17 @@ public class Piloto {
     private int constructorId;
     private String url;
 
+    /**
+     * Constructor de Piloto
+     * @param driverId ID del piloto
+     * @param driverCode Código del piloto
+     * @param driverForename Nombre del piloto
+     * @param driverSurname Apellido del piloto
+     * @param driverDOB Fecha de nacimiento del piloto
+     * @param driverNationality Nacionalidad del piloto
+     * @param constructorId ID del constructor
+     * @param url URL del piloto
+     */
     public Piloto(int driverId, String driverCode, String driverForename, String driverSurname, String driverDOB, String driverNationality, int constructorId, String url) {
         this.driverId = driverId;
         this.driverCode = driverCode;
