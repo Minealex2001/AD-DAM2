2d96a80b5017b32c9e295e4e3a9ff5fc6a8a7c9e
Minealex <alejandrosanchezpinto@gmail.com>
2023-11-28 19:44:46 +0100

    Lo termino en casa

--- Tema-3/ejercicio-3/src/main/java/db/DBConnection.java
+++ Tema-3/ejercicio-3/src/main/java/db/DBConnection.java
@@ -1,7 +1,24 @@
 package db;
 
-import java.sql.Connection;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import entities.Drivers;
+import org.bson.codecs.configuration.CodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+
+import static com.mongodb.MongoClientSettings.getDefaultCodecRegistry;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
 
 public class DBConnection {
-    public MongoClient dbConnection(String)
+    public MongoCollection<Drivers> dbConnection(String username, String password, String ip){
+        MongoClient dbClient = MongoClients.create("mongodb://" + username + ":" + password + "@" + ip + ":27017");
+        CodecProvider pojoCodecProvider = PojoCodecProvider.builder().automatic(true).build();
+        CodecRegistry pojoCodecRegistry = fromRegistries(getDefaultCodecRegistry(), fromProviders(pojoCodecProvider));
+        MongoDatabase db = dbClient.getDatabase("f1-2006").withCodecRegistry(pojoCodecRegistry);
+        return db.getCollection("drivers", Drivers.class);
+    }
 }
